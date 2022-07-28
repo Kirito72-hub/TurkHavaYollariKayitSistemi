05ff10835660dc31206f766cae20c4b63fa10833
yasirstaj <caliskanstaj@gmail.com>
2022-07-28 08:18:41 +0300

    Add project files.

---
+++ TurkHavaYollarıKayıtSistemi/Forms/GirisFormu.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace TurkHavaYollarıKayıtSistemi
+{
+    partial class GirisFormu
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GirisFormu));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtboxSifre = new System.Windows.Forms.TextBox();
+            this.txtboxKullanci = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(971, 684);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(103, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MERHABA";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(100, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(567, 59);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nereyi keşfetmek istersiniz?";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txtboxSifre);
+            this.panel2.Controls.Add(this.txtboxKullanci);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(971, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(466, 684);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(69, 579);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(315, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Giriş yap";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(65, 487);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sifrenizi unuttunuz mu?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(64, 404);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 26);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Sifre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(64, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 26);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kullancı Adı";
+            // 
+            // txtboxSifre
+            // 
+            this.txtboxSifre.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtboxSifre.ForeColor = System.Drawing.Color.Black;
+            this.txtboxSifre.Location = new System.Drawing.Point(69, 432);
+            this.txtboxSifre.Name = "txtboxSifre";
+            this.txtboxSifre.Size = new System.Drawing.Size(315, 43);
+            this.txtboxSifre.TabIndex = 3;
+            this.txtboxSifre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtboxKullanci
+            // 
+            this.txtboxKullanci.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtboxKullanci.ForeColor = System.Drawing.Color.Black;
+            this.txtboxKullanci.Location = new System.Drawing.Point(69, 334);
+            this.txtboxKullanci.Name = "txtboxKullanci";
+            this.txtboxKullanci.Size = new System.Drawing.Size(315, 43);
+            this.txtboxKullanci.TabIndex = 1;
+            this.txtboxKullanci.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(29, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(407, 113);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(421, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // GirisFormu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1437, 684);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "GirisFormu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtboxKullanci;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtboxSifre;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TurkHavaYollarıKayıtSistemi/Forms/GirisFormu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using TurkHavaYollarıKayıtSistemi;
+
+namespace TurkHavaYollarıKayıtSistemi
+{
+    public partial class GirisFormu : Form
+    {
+        public GirisFormu()
+        {
+            InitializeComponent();
+        }
+        //databasein nesnesi olusturmasi
+        SqlConnection db = new SqlConnection("Data Source=LAPTOP-SCMK2VA2\\MSSQLSERVER01;Initial Catalog=THY;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.FromArgb(232, 25, 50);
+        }
+        
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Giris bilgileri kontrol edip hataise hata vercek dogru ise yeni form cagiracak
+            db.Open();
+            SqlDataAdapter sorgu = new SqlDataAdapter("Select Count(*) From Tbl_Kullanci Where Kullanci='" + txtboxKullanci.Text + "' And Sifre='" + txtboxSifre.Text + "'", db);
+            DataTable dt = new DataTable();
+            sorgu.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                //Sifre dogru ise
+                ConstaValue.KullanciAdi = txtboxKullanci.Text;
+                this.Hide();
+                Dashboard dash = new Dashboard();
+                dash.Show();
+            }
+            else
+                MessageBox.Show("Kullanci Adı ve Şifresi giriniz !!", "Bütün boşlukları doldurun", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            db.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+//Sifremi unuttum kismi
