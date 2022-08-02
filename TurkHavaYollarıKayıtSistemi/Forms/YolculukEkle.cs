7d805e8e640c15ade75c51aead2d858290a6344b
Ahmed Saudi <ahmad.moayad@hotmail.com>
2022-08-02 22:33:52 +0300

    Revert "Fixed save(kaydet) button in yolculukekle form working perfectly"
    This reverts commit adaabca15c3e21413b833a856884409d53f184c2.

--- TurkHavaYollarıKayıtSistemi/Forms/YolculukEkle.cs
+++ TurkHavaYollarıKayıtSistemi/Forms/YolculukEkle.cs
@@ -33,14 +33,13 @@ namespace TurkHavaYollarıKayıtSistemi.Forms
                 if(!cbUcakID.Items.Contains(name))
                     cbUcakID.Items.Add(name);
             }
-            db.Close();
         }
 
         private void btnIptal_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-        //kayidetme tusu basildiginda girdileri alip databasete skalmak icin
+
         private void btnKaydet_Click(object sender, EventArgs e)
         {
             bool flick = true;
@@ -53,9 +52,6 @@ namespace TurkHavaYollarıKayıtSistemi.Forms
                 if(dialogResult == DialogResult.Yes)
                 {
                     db.Open();
-                    string strUcakAdi = cbUcakID.SelectedItem.ToString();
-                    SqlCommand IDgetir = new SqlCommand("Select UcakID From Tbl_Ucak Where UcakModeli='" + strUcakAdi + "'", db);
-                    Int32 UcakID = (Int32)IDgetir.ExecuteScalar();
                     SqlCommand sorgu = new SqlCommand("Insert into Tbl_Yolculuk (Nerden, Nereye, GidisTarihi, GidisZamani, VarisTarihi, VarisZamani, UcakID) values(@s1, @s2, @s3, @s4, @s5, @s6, @s7)", db);
                     sorgu.Parameters.AddWithValue("@s1", cbNerden.Text);
                     sorgu.Parameters.AddWithValue("@s2", cbNereye.Text);
@@ -63,7 +59,7 @@ namespace TurkHavaYollarıKayıtSistemi.Forms
                     sorgu.Parameters.AddWithValue("@s4", dtpKalkisSaati.Value);
                     sorgu.Parameters.AddWithValue("@s5", dtpVarisTarihi.Value);
                     sorgu.Parameters.AddWithValue("@s6", dtpVarisSaati.Value);
-                    sorgu.Parameters.AddWithValue("@s7", UcakID);
+                    sorgu.Parameters.AddWithValue("@s7", cbUcakID);
                     sorgu.ExecuteNonQuery();
 
                     db.Close();
