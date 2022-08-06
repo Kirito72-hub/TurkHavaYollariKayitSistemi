1070e96f9a69727ba463fb28664969c94b4e684a
Ahmed Saudi <ahmad.moayad@hotmail.com>
2022-08-06 14:06:33 +0300

    +KC_Yolcu buttons coded

--- TurkHavaYollarıKayıtSistemi/KullanciKontrolUi/KC_Yolcu.cs
+++ TurkHavaYollarıKayıtSistemi/KullanciKontrolUi/KC_Yolcu.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using TurkHavaYollarıKayıtSistemi.Forms;
 
 namespace TurkHavaYollarıKayıtSistemi.KullanciKontrolUi
 {
@@ -15,6 +17,87 @@ namespace TurkHavaYollarıKayıtSistemi.KullanciKontrolUi
         public KC_Yolcu()
         {
             InitializeComponent();
+        }
+        //sql baglantisini kurmak
+        SqlConnection db = new SqlConnection(ConstValue.connectionString);
+
+        //datagridview1 headers basliklari degistirmek
+        private void FillDataGridView()
+        {
+            dataGridView1.Columns[0].HeaderText = "Yolcu No";
+            dataGridView1.Columns[0].AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[1].HeaderText = "Ad";
+            dataGridView1.Columns[1].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[2].HeaderText = "Soyad";
+            dataGridView1.Columns[2].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[3].HeaderText = "E-posta";
+            dataGridView1.Columns[3].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[4].HeaderText = "Telefon";
+            dataGridView1.Columns[4].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[5].HeaderText = "Kayıt Tarihi";
+            dataGridView1.Columns[5].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.AutoResizeColumnHeadersHeight();
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.ClearSelection();
+        }
+        //formun font style degitirmek icin
+        private void UpdateFont()
+        {
+            foreach (DataGridViewColumn c in dataGridView1.Columns)
+            {
+                c.DefaultCellStyle.Font = new Font("Arial", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            }
+        }
+
+        private void KC_Yolcu_Load(object sender, EventArgs e)
+        {
+            //tabloyu ekrana yazmak
+            db.Open();
+            SqlDataAdapter sorgu = new SqlDataAdapter("Select * from Tbl_Yolcu", db);
+            DataTable dt = new DataTable();
+            sorgu.Fill(dt);
+            dataGridView1.DataSource = dt;
+            FillDataGridView();
+            UpdateFont();
+
+            //yolculuk sayisi toplayip label icinde yazmak
+            SqlCommand YolcuSayisi = new SqlCommand("Select count(*) from Tbl_Ucak", db);
+            labelYolcuSayisi.Text = YolcuSayisi.ExecuteScalar().ToString();
+            db.Close();
+        }
+        private void btnYolcuEkle_Click(object sender, EventArgs e)
+        {
+            //Ucak Ekle kodu formu cagirmak
+            using (YolcuEkle YolcuEkle = new YolcuEkle())
+            {
+                YolcuEkle.ShowDialog();
+                this.OnLoad(e);
+            }
+        }
+        //tablodan bir cell uzerinde basarsak silmek butonu cikar silme basarsak kayit siliniyor
+        int Scell;
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnYolcuSil.Visible = true;
+            int index = dataGridView1.CurrentCell.RowIndex;
+            object value = dataGridView1.Rows[index].Cells[0].Value;
+            Scell = (int)value;
+        }
+        //tablodan bir cell uzerinde basarsak silmek butonu cikar silme basarsak kayit siliniyor
+        private void btnYolcuSil_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Kaydı silmek istiyor musunuz?", "confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes)
+            {
+                db.Open();
+                SqlCommand sorguSil = new SqlCommand("delete from Tbl_Yolcu where YolcuID=@s1", db);
+                sorguSil.Parameters.AddWithValue("@s1", Scell);
+                sorguSil.ExecuteNonQuery();
+                db.Close();
+                MessageBox.Show("Yolcu Kaydı Silindi");
+                this.OnLoad(e);
+            }
         }
     }
 }
