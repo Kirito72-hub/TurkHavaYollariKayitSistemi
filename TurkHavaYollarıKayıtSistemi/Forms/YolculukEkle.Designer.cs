d21f76300e3d5c75680286d3a299993a46162f10
Ahmed Saudi <ahmad.moayad@hotmail.com>
2022-08-06 12:58:39 +0300

    -Some quality of life fixes on YolculukEkle desgin

--- TurkHavaYollarıKayıtSistemi/Forms/YolculukEkle.Designer.cs
+++ TurkHavaYollarıKayıtSistemi/Forms/YolculukEkle.Designer.cs
@@ -67,7 +67,7 @@
             this.panel1.Location = new System.Drawing.Point(9, 9);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(778, 642);
+            this.panel1.Size = new System.Drawing.Size(918, 642);
             this.panel1.TabIndex = 0;
             // 
             // dtpVarisSaati
@@ -75,10 +75,10 @@
             this.dtpVarisSaati.CustomFormat = "HH:mm";
             this.dtpVarisSaati.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.dtpVarisSaati.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtpVarisSaati.Location = new System.Drawing.Point(411, 315);
+            this.dtpVarisSaati.Location = new System.Drawing.Point(482, 305);
             this.dtpVarisSaati.Name = "dtpVarisSaati";
             this.dtpVarisSaati.ShowUpDown = true;
-            this.dtpVarisSaati.Size = new System.Drawing.Size(265, 43);
+            this.dtpVarisSaati.Size = new System.Drawing.Size(412, 43);
             this.dtpVarisSaati.TabIndex = 4;
             this.dtpVarisSaati.Value = new System.DateTime(2022, 7, 28, 16, 35, 0, 0);
             // 
@@ -87,9 +87,9 @@
             this.dtpVarisTarihi.CustomFormat = "";
             this.dtpVarisTarihi.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.dtpVarisTarihi.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpVarisTarihi.Location = new System.Drawing.Point(411, 251);
+            this.dtpVarisTarihi.Location = new System.Drawing.Point(482, 241);
             this.dtpVarisTarihi.Name = "dtpVarisTarihi";
-            this.dtpVarisTarihi.Size = new System.Drawing.Size(265, 43);
+            this.dtpVarisTarihi.Size = new System.Drawing.Size(412, 43);
             this.dtpVarisTarihi.TabIndex = 4;
             this.dtpVarisTarihi.Value = new System.DateTime(2022, 7, 28, 16, 35, 0, 0);
             // 
@@ -98,28 +98,28 @@
             this.dtpKalkisSaati.CustomFormat = "HH:mm";
             this.dtpKalkisSaati.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.dtpKalkisSaati.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtpKalkisSaati.Location = new System.Drawing.Point(21, 315);
+            this.dtpKalkisSaati.Location = new System.Drawing.Point(32, 305);
             this.dtpKalkisSaati.Name = "dtpKalkisSaati";
             this.dtpKalkisSaati.ShowUpDown = true;
-            this.dtpKalkisSaati.Size = new System.Drawing.Size(265, 43);
+            this.dtpKalkisSaati.Size = new System.Drawing.Size(417, 43);
             this.dtpKalkisSaati.TabIndex = 4;
             // 
             // dtpKalkisTarihi
             // 
             this.dtpKalkisTarihi.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.dtpKalkisTarihi.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpKalkisTarihi.Location = new System.Drawing.Point(21, 251);
+            this.dtpKalkisTarihi.Location = new System.Drawing.Point(32, 241);
             this.dtpKalkisTarihi.Name = "dtpKalkisTarihi";
-            this.dtpKalkisTarihi.Size = new System.Drawing.Size(265, 43);
+            this.dtpKalkisTarihi.Size = new System.Drawing.Size(417, 43);
             this.dtpKalkisTarihi.TabIndex = 4;
             // 
             // cbUcakID
             // 
             this.cbUcakID.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.cbUcakID.FormattingEnabled = true;
-            this.cbUcakID.Location = new System.Drawing.Point(21, 413);
+            this.cbUcakID.Location = new System.Drawing.Point(32, 403);
             this.cbUcakID.Name = "cbUcakID";
-            this.cbUcakID.Size = new System.Drawing.Size(265, 41);
+            this.cbUcakID.Size = new System.Drawing.Size(417, 41);
             this.cbUcakID.TabIndex = 3;
             this.cbUcakID.Text = "Uçak Numerası";
             // 
@@ -168,9 +168,9 @@
             "Uşak Havalimanı",
             "Van Ferit Melen Havalimanı",
             "Zonguldak Çaycuma Havalimanı"});
-            this.cbNereye.Location = new System.Drawing.Point(411, 129);
+            this.cbNereye.Location = new System.Drawing.Point(477, 129);
             this.cbNereye.Name = "cbNereye";
-            this.cbNereye.Size = new System.Drawing.Size(353, 41);
+            this.cbNereye.Size = new System.Drawing.Size(417, 41);
             this.cbNereye.TabIndex = 3;
             // 
             // cbNerden
@@ -219,9 +219,9 @@
             "Uşak Havalimanı",
             "Van Ferit Melen Havalimanı",
             "Zonguldak Çaycuma Havalimanı"});
-            this.cbNerden.Location = new System.Drawing.Point(21, 129);
+            this.cbNerden.Location = new System.Drawing.Point(32, 129);
             this.cbNerden.Name = "cbNerden";
-            this.cbNerden.Size = new System.Drawing.Size(353, 41);
+            this.cbNerden.Size = new System.Drawing.Size(417, 41);
             this.cbNerden.TabIndex = 2;
             // 
             // btnIptal
@@ -230,7 +230,7 @@
             this.btnIptal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnIptal.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnIptal.ForeColor = System.Drawing.Color.Red;
-            this.btnIptal.Location = new System.Drawing.Point(392, 540);
+            this.btnIptal.Location = new System.Drawing.Point(462, 540);
             this.btnIptal.Name = "btnIptal";
             this.btnIptal.Size = new System.Drawing.Size(184, 53);
             this.btnIptal.TabIndex = 1;
@@ -244,7 +244,7 @@
             this.btnKaydet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnKaydet.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnKaydet.ForeColor = System.Drawing.Color.LimeGreen;
-            this.btnKaydet.Location = new System.Drawing.Point(202, 540);
+            this.btnKaydet.Location = new System.Drawing.Point(272, 540);
             this.btnKaydet.Name = "btnKaydet";
             this.btnKaydet.Size = new System.Drawing.Size(184, 53);
             this.btnKaydet.TabIndex = 1;
@@ -257,7 +257,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(43)))), ((int)(((byte)(56)))));
-            this.label2.Location = new System.Drawing.Point(272, 16);
+            this.label2.Location = new System.Drawing.Point(334, 16);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(250, 39);
             this.label2.TabIndex = 0;
@@ -267,7 +267,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(404, 87);
+            this.label5.Location = new System.Drawing.Point(475, 87);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(112, 39);
             this.label5.TabIndex = 0;
@@ -277,7 +277,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(404, 209);
+            this.label1.Location = new System.Drawing.Point(475, 199);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(163, 39);
             this.label1.TabIndex = 0;
@@ -287,7 +287,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(14, 87);
+            this.label3.Location = new System.Drawing.Point(25, 87);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(115, 39);
             this.label3.TabIndex = 0;
@@ -297,7 +297,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(14, 209);
+            this.label4.Location = new System.Drawing.Point(25, 199);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(176, 39);
             this.label4.TabIndex = 0;
@@ -308,7 +308,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(43)))), ((int)(((byte)(56)))));
-            this.ClientSize = new System.Drawing.Size(796, 660);
+            this.ClientSize = new System.Drawing.Size(936, 660);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "YolculukEkle";
